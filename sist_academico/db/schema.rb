1bc3507f7ee8c83c5062436bdf1f4c7f18fd693c
Mirian Saucedo <miriansau@gmail.com>
2013-05-27 17:18:32 -0400

    CSS de Registrar Curso
    #52 Se mejora la interfaz del registro de un nuevo curso, que comprende
    trabajar mejor con la ubicación de los label, textfield, etc.

--- sist_academico/db/schema.rb
+++ sist_academico/db/schema.rb
@@ -36,10 +36,10 @@ ActiveRecord::Schema.define(:version => 20130520233801) do
     t.boolean  "doc_cert_estudios"
     t.boolean  "doc_foto"
     t.boolean  "doc_cert_nacimiento"
-    t.datetime "created_at",                         :null => false
-    t.datetime "updated_at",                         :null => false
-    t.string   "responsable",          :limit => 30, :null => false
-    t.string   "telefono_responsable", :limit => 15, :null => false
+    t.datetime "created_at",           :null => false
+    t.datetime "updated_at",           :null => false
+    t.string   "responsable"
+    t.string   "telefono_responsable"
   end
 
   create_table "cities", :force => true do |t|
@@ -88,6 +88,7 @@ ActiveRecord::Schema.define(:version => 20130520233801) do
     t.string   "telefono"
     t.string   "fecha_nacimiento"
     t.string   "lugar_nacimiento"
+    t.string   "direccion"
     t.integer  "edad"
     t.string   "username",                     :null => false
     t.string   "email"
