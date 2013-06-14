0923bf848284e6c52213542797c53a8962a7f211
Mirian Saucedo <miriansau@gmail.com>
2013-06-14 17:14:44 -0400

    Valores Límites de Registro de Usuario
    #68 Valores Límites Corregidos según los resultados obtenidos e en el
    caso de prueba realizado

--- sist_academico/app/models/user.rb
+++ sist_academico/app/models/user.rb
@@ -7,18 +7,20 @@ class User < ActiveRecord::Base
   has_one :administrativo
   has_one :docente
   before_save { |user| user.email = email.downcase }
-  validates :username, presence: true, length: { maximum: 50 }
-  validates :CINro, presence: true, length: { maximum: 9 }
+
+  #VALIDACIONES DE LOS CAMPOS
+  validates :username, presence: true, length: { maximum: 50, minimum:2 }
+  validates :CINro, presence: true, length: { maximum: 9, minimum:6 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence:   true,
                     format:     { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { maximum: 20, minimum: 6 }
   validates_confirmation_of :password
-  validates :nombre, presence: true, length: { maximum: 50 }
-  validates :apellido, presence: true, length: { maximum: 50 }
+  validates :nombre, presence: true, length: { maximum: 50, minimum:3 }
+  validates :apellido, presence: true, length: { maximum: 50, minimum:3 }
   validates :sexo, presence: true, length: { maximum: 10 }
-  validates :telefono, presence: true, length: { maximum: 50 }
+  validates :telefono, presence: true, length: { maximum: 50, minimum:6 }
   validates :fecha_nacimiento, presence: true, length: { maximum: 50 }
   validates :lugar_nacimiento, presence: true
   validates :address_id, presence: true, length: { maximum: 50 }
