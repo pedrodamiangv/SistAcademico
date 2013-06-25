f18ceb762de039e75bb668b1f1e6ae931c0491f2
Mirian Saucedo <miriansau@gmail.com>
2013-06-25 18:15:43 -0400

    Auditoría
    #70 #72 Auditoría de los ABMs de Alumnos, Docentes y Administrativos. Y
    algunas pequeñas modificaciones en materias y el form de docentes.

--- sist_academico/app/controllers/alumnos_controller.rb
+++ sist_academico/app/controllers/alumnos_controller.rb
@@ -1,3 +1,4 @@
+require 'custom_logger'
 class AlumnosController < ApplicationController
   before_filter :require_login
   before_filter :correct_user || :admin_user,   only: [:edit, :update, :show] 
@@ -76,12 +77,14 @@ class AlumnosController < ApplicationController
           edad = calcular_edad user
           user.update_attribute(:is_alumno, true)
           user.update_attribute(:edad, edad )
-          format.html { redirect_to @alumno, notice: 'El alumno ha sido registrado.' }
+          format.html { redirect_to @alumno, notice: 'El alumno ha sido registrado con exito.' }
+          CustomLogger.info("Nuevo alumno: #{@alumno.user_nombre.inspect} ,Apellido: #{@alumno.user_apellido.inspect} ,Cedula de Identidad: #{@alumno.user_CINro.inspect} ,Sexo: #{@alumno.user_sexo.inspect} ,Telefono:#{@alumno.user_telefono.inspect} ,Correo Electronico: #{@alumno.user_email.inspect} ,Fecha de Nacimiento:#{@alumno.user_fecha_nacimiento.inspect} ,Lugar de Nacimiento: #{@alumno.user_lugar_nacimiento.inspect} ,Nombre del Responsable: #{@alumno.responsable.inspect} ,Telefono del Responsable: #{@alumno.telefono_responsable.inspect} ,Direccion: #{@alumno.user.address.direccion.inspect} ,Documentos Personales: Certificado de Estudio: #{@alumno.doc_cert_estudios.inspect} ,Cedula: #{@alumno.doc_cedula.inspect} ,Certificado de Nacimiento: #{@alumno.doc_cert_nacimiento.inspect} ,Foto Tipo Carnet: #{@alumno.doc_foto.inspect} ,Nombre de Usuario: #{@alumno.user_username.inspect} creado por el usuario: #{current_user.full_name.inspect} ,#{Time.now}")
           format.json { render json: @alumno, status: :created, location: @alumno }
           format.js   {}
         else
           atributos
           format.html { render action: "new" }
+          CustomLogger.error("Error al querer crear el nuevo alumno: #{@alumno.user_nombre.inspect} y sus demas atributos, por el usuario: #{current_user.full_name.inspect} ,#{Time.now}")
           format.json { render json: @alumno.errors, status: :unprocessable_entity }
         end
     end
@@ -91,9 +94,42 @@ class AlumnosController < ApplicationController
   # PUT /alumnos/1.json
   def update
     @alumno = Alumno.find(params[:id])
+    nombre_antiguo = @alumno.user_nombre
+    apellido_antiguo = @alumno.user_apellido
+    cedula_antiguo = @alumno.user_CINro
+    sexo_antiguo = @alumno.user_sexo
+    telefono_antiguo = @alumno.user_telefono
+    correo_antiguo = @alumno.user_email
+    fechaNac_antiguo = @alumno.user_fecha_nacimiento
+    lugarNac_antiguo = @alumno.user_lugar_nacimiento
+    direccion_antiguo = @alumno.user.address.direccion
+    responsable_antiguo = @alumno.responsable
+    telefonoRespon_antiguo = @alumno.telefono_responsable
+    doc_cedula_antiguo = @alumno.doc_cedula
+    doc_cert_estudios_antiguo = @alumno.doc_cert_estudios
+    doc_cert_nacimiento_antiguo = @alumno.doc_cert_nacimiento
+    doc_foto_antiguo = @alumno.doc_foto
+    nombreUsuario_antiguo = @alumno.user_username
 
     respond_to do |format|
       if @alumno.update_attributes(params[:alumno])
+        nombre_nuevo = @alumno.user_nombre
+        apellido_nuevo = @alumno.user_apellido
+        cedula_nuevo = @alumno.user_CINro
+        sexo_nuevo = @alumno.user_sexo
+        telefono_nuevo = @alumno.user_telefono
+        correo_nuevo = @alumno.user_email
+        fechaNac_nuevo = @alumno.user_fecha_nacimiento
+        lugarNac_nuevo = @alumno.user_lugar_nacimiento
+        direccion_nuevo = @alumno.user.address.direccion
+        responsable_nuevo = @alumno.responsable
+        telefonoRespon_nuevo = @alumno.telefono_responsable
+        doc_cedula_nuevo = @alumno.doc_cedula
+        doc_cert_estudios_nuevo = @alumno.doc_cert_estudios
+        doc_cert_nacimiento_nuevo = @alumno.doc_cert_nacimiento
+        doc_foto_nuevo = @alumno.doc_foto
+        nombreUsuario_nuevo = @alumno.user_username
+        CustomLogger.info("Los datos antes de ser actualizados son: Nombre del alumno: #{nombre_antiguo.inspect} ,Apellido: #{apellido_antiguo.inspect} ,Cedula de Identidad: #{cedula_antiguo.inspect} ,Sexo: #{sexo_antiguo.inspect} ,Telefono:#{telefono_antiguo.inspect} ,Correo Electronico: #{correo_antiguo.inspect} ,Fecha de Nacimiento:#{fechaNac_antiguo.inspect} ,Lugar de Nacimiento: #{lugarNac_antiguo.inspect} ,Nombre del Responsable: #{responsable_antiguo.inspect} ,Telefono del Responsable: #{telefonoRespon_antiguo.inspect} ,Direccion: #{direccion_antiguo.inspect} ,Documentos Personales: Foto Tipo Carnet: #{doc_foto_antiguo.inspect} ,Cedula: #{doc_cedula_antiguo.inspect} ,Certificado de Nacimiento: #{doc_cert_nacimiento_antiguo.inspect} ,Certificado de Estudio: #{doc_cert_estudios_antiguo.inspect} ,Nombre de Usuario: #{nombreUsuario_antiguo.inspect} .Los datos actualizados por el usuario: #{current_user.full_name.inspect} son los siguientes: Nombre del alumno: #{nombre_nuevo.inspect} ,Apellido: #{apellido_nuevo.inspect} ,Cedula de Identidad: #{cedula_nuevo.inspect} ,Sexo: #{sexo_nuevo.inspect} ,Telefono:#{telefono_nuevo.inspect} ,Correo Electronico: #{correo_nuevo.inspect} ,Fecha de Nacimiento:#{fechaNac_nuevo.inspect} ,Lugar de Nacimiento: #{lugarNac_nuevo.inspect} ,Nombre del Responsable: #{responsable_nuevo.inspect} ,Telefono del Responsable: #{telefonoRespon_nuevo.inspect} ,Direccion: #{direccion_nuevo.inspect} ,Documentos Personales: Foto Tipo Carnet: #{doc_foto_nuevo.inspect} ,Cedula: #{doc_cedula_nuevo.inspect} ,Certificado de Nacimiento: #{doc_cert_nacimiento_nuevo.inspect} ,Certificado de Estudio: #{doc_cert_estudios_nuevo.inspect} ,Nombre de Usuario: #{nombreUsuario_nuevo.inspect} ,#{Time.now} ")
         format.html { redirect_to @alumno, notice: 'El alumno ha sido actualizado.' }
         format.json { head :no_content }
       else
@@ -108,11 +144,18 @@ class AlumnosController < ApplicationController
   # DELETE /alumnos/1.json
   def destroy
     @alumno = Alumno.find(params[:id])
-    @alumno.destroy
-
     respond_to do |format|
-      format.html { redirect_to alumnos_url }
-      format.json { head :no_content }
+      begin
+        @alumno.destroy
+        notice = "El alumnos y sus demas atributos han sido eliminados correctamente. "
+        CustomLogger.info("Nombre del alumno: #{@alumno.user_nombre.inspect} ,Apellido: #{@alumno.user_apellido.inspect} ,Cedula de Identidad: #{@alumno.user_CINro.inspect} ,Sexo: #{@alumno.user_sexo.inspect} ,Telefono:#{@alumno.user_telefono.inspect} ,Correo Electronico: #{@alumno.user_email.inspect} ,Fecha de Nacimiento:#{@alumno.user_fecha_nacimiento.inspect} ,Lugar de Nacimiento: #{@alumno.user_lugar_nacimiento.inspect} ,Nombre del Responsable: #{@alumno.responsable.inspect} ,Telefono del Responsable: #{@alumno.telefono_responsable.inspect} ,Direccion: #{@alumno.user.address.direccion.inspect} ,Documentos Personales: Certificado de Estudio: #{@alumno.doc_cert_estudios.inspect} ,Cedula: #{@alumno.doc_cedula.inspect} ,Certificado de Nacimiento: #{@alumno.doc_cert_nacimiento.inspect} ,Foto Tipo Carnet: #{@alumno.doc_foto.inspect} ,Nombre de Usuario: #{@alumno.user_username.inspect} han sido eliminados por el usuario: #{current_user.full_name.inspect} ,#{Time.now}")
+      rescue
+        notice = "Este alumnos y sus demas atributos no pueden ser eliminados"
+        CustomLogger.error("Error al eliminar el alumno: Nombre del alumno: #{@alumno.user_nombre.inspect} y sus demas atributos, por el usuario: #{current_user.full_name.inspect} ,#{Time.now} ")
+      ensure
+        format.html { redirect_to alumnos_url }
+        format.json { head :no_content }
+      end
     end
   end
 
