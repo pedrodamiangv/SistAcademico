43a1d5a4d75b9f6dc4bddf6c5603f2c4527ac59d
Mirian Saucedo <miriansau@gmail.com>
2013-06-25 20:31:58 -0400

    Auditoría
    #70 #72 Auditoria del ultimo ABM faltante el de usuarios.

--- sist_academico/app/controllers/users_controller.rb
+++ sist_academico/app/controllers/users_controller.rb
@@ -1,3 +1,4 @@
+require 'custom_logger'
 class UsersController < ApplicationController
   before_filter :require_login
   before_filter :correct_user,   only: [:edit, :update, :show]
@@ -20,7 +21,8 @@ class UsersController < ApplicationController
       if @user.save
         @user.edad = calcular_edad @user
         user.update_attribute(:edad, edad )
-        format.html { redirect_to @user, notice: 'El Usuario ha sido registrado.' }
+        format.html { redirect_to @user, notice: 'El Usuario ha sido registrado con exito.' }
+        CustomLogger.info("Nuevo Usuario: #{@user_nombre.inspect} ,Apellido: #{@user_apellido.inspect} ,Cedula de Identidad: #{@user_CINro.inspect} ,Sexo: #{@user_sexo.inspect} ,Telefono:#{@user_telefono.inspect} ,Correo Electronico: #{@user_email.inspect} ,Fecha de Nacimiento:#{@user_fecha_nacimiento.inspect} ,Lugar de Nacimiento: #{@user_lugar_nacimiento.inspect} ,Direccion: #{@user.address.direccion.inspect} ,Nombre de Usuario: #{@user_username.inspect} creado por el usuario: #{current_user.full_name.inspect} ,#{Time.now}")
         format.json { render json: @user, status: :created, location: @user }
         format.js   {}
       else
@@ -28,6 +30,7 @@ class UsersController < ApplicationController
         @cities_city = cities
         addresses_new
         format.html { render action: "new" }
+        CustomLogger.error("Error al querer crear el usuario: #{@user_nombre.inspect} y sus demas atributos, por el usuario: #{current_user.full_name.inspect}, #{Time.now}")
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
@@ -42,9 +45,32 @@ class UsersController < ApplicationController
 
   def update
     @user = User.new (params[:user])
+    nombre_antiguo = @user_nombre
+    apellido_antiguo = @user_apellido
+    cedula_antiguo = @user_CINro
+    sexo_antiguo = @user_sexo
+    telefono_antiguo = @user_telefono
+    correo_antiguo = @user_email
+    fechaNac_antiguo = @user_fecha_nacimiento
+    lugarNac_antiguo = @user_lugar_nacimiento
+    direccion_antiguo = @user.address.direccion
+    nombreUsuario_antiguo = @user_username
+
     respond_to do |format|
       if @user.update_attributes(params[:user])
-        format.html { redirect_to @user, notice: 'Los campos han sido actualizados.' }   
+        nombre_nuevo = @user_nombre
+        apellido_nuevo = @user_apellido
+        cedula_nuevo = @user_CINro
+        sexo_nuevo = @user_sexo
+        telefono_nuevo = @user_telefono
+        correo_nuevo = @user_email
+        fechaNac_nuevo = @user_fecha_nacimiento
+        lugarNac_nuevo = @user_lugar_nacimiento
+        direccion_nuevo = @user.address.direccion
+        nombreUsuario_nuevo = @user_username
+
+        CustomLogger.info("Los datos antes de ser actualizados son: Nombre: #{nombre_antiguo.inspect} ,Apellido: #{apellido_antiguo.inspect} ,Cedula de Identidad: #{cedula_antiguo.inspect} ,Sexo: #{sexo_antiguo.inspect} ,Telefono:#{telefono_antiguo.inspect} ,Correo Electronico: #{correo_antiguo.inspect} ,Fecha de Nacimiento:#{fechaNac_antiguo.inspect} ,Lugar de Nacimiento: #{lugarNac_antiguo.inspect} ,Direccion: #{direccion_antiguo.inspect} ,Nombre de Usuario: #{nombreUsuario_antiguo.inspect} .Los datos actualizados por el usuario: #{current_user.full_name.inspect} son los siguientes: #{nombre_nuevo.inspect} ,Apellido: #{apellido_nuevo.inspect} ,Cedula de Identidad: #{cedula_nuevo.inspect} ,Sexo: #{sexo_nuevo.inspect} ,Telefono:#{telefono_nuevo.inspect} ,Correo Electronico: #{correo_nuevo.inspect} ,Fecha de Nacimiento:#{fechaNac_nuevo.inspect} ,Lugar de Nacimiento: #{lugarNac_nuevo.inspect} ,Direccion: #{direccion_nuevo.inspect} ,Nombre de Usuario: #{nombreUsuario_nuevo.inspect}, #{Time.now}")
+        format.html { redirect_to @user, notice: 'Los campos han sido actualizados correctamente.' }   
       else
         @addresses = Address.order("created_at desc").find(:all)
         @cities_city = cities
@@ -61,11 +87,18 @@ class UsersController < ApplicationController
 
   def destroy
     @user = User.find(params[:id])
-    @user.destroy
-
     respond_to do |format|
-      format.html { redirect_to users_url }
-      format.json { head :no_content }
+      begin
+        @user.destroy
+        notice = "El usuario y sus demas atributos han sido eliminado correctamente. "
+        CustomLogger.info("Usuario: #{@user_nombre.inspect} ,Apellido: #{@user_apellido.inspect} ,Cedula de Identidad: #{@user_CINro.inspect} ,Sexo: #{@user_sexo.inspect} ,Telefono:#{@user_telefono.inspect} ,Correo Electronico: #{@user_email.inspect} ,Fecha de Nacimiento:#{@user_fecha_nacimiento.inspect} ,Lugar de Nacimiento: #{@user_lugar_nacimiento.inspect} ,Direccion: #{@user.address.direccion.inspect} ,Nombre de Usuario: #{@user_username.inspect} han sido eliminados por el usuario: #{current_user.full_name.inspect} ,#{Time.now}")
+      rescue
+        notice = "Este usuario y sus demas atributos no pueden ser eliminados"
+        CustomLogger.error("Error al eliminar el usuario: #{@user_nombre.inspect} y sus demas atributos, por el usuario: #{current_user.full_name.inspect}, #{Time.now} ")
+      ensure
+        format.html { redirect_to users_url, notice: notice }
+        format.json { head :no_content }
+      end
     end
   end
 
