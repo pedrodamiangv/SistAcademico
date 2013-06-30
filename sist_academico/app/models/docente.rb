fe0889edb1d609d10d6efb580b4bbc668e6d314a
Mirian Saucedo <miriansau@gmail.com>
2013-06-29 21:59:11 -0400

    CSS
    #79  Css de Docentes y Sessions

--- sist_academico/app/models/docente.rb
+++ sist_academico/app/models/docente.rb
@@ -5,7 +5,7 @@ class Docente < ActiveRecord::Base
   delegate :area, :materia, to: :materia, prefix: true
   accepts_nested_attributes_for :user
   delegate :CINro, :nombre, :apellido, :sexo, :telefono, :fecha_nacimiento, :lugar_nacimiento, :edad, :username, :email, :password, :password_confirmation, :address_id, to: :user, prefix: true
-  validates :matricula, presence: true, length: {minimum: 5, maximum: 20}, uniqueness: true, :format => { :with => /\d/}
+  validates :matricula, presence: true, length: {minimum: 5, maximum: 15}, uniqueness: true, :format => { :with => /\d/}
   validates :titulo, presence: true, length: {minimum: 5, maximum: 50}, :format => { :with => /\A[a-zA-Z\s]+\z/ }
   before_save { build_user unless user }
   def full_name
