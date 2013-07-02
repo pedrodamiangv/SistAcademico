b4cced678b8d174be816dfe7e8466912a5b9be19
Mirian Saucedo <miriansau@gmail.com>
2013-07-02 06:39:24 -0400

    Modelo
    #79 Algunas correcciones en el modelo

--- sist_academico/app/models/country.rb
+++ sist_academico/app/models/country.rb
@@ -1,5 +1,5 @@
 class Country < ActiveRecord::Base
   attr_accessible :pais
   has_many :cities
-  validates :pais, presence: true, length: { maximum: 30, minimum:3 }, :format => { :with => /\A[a-zA-Z\s]+\z/ }
+  validates :pais, presence: true, length: { maximum: 30, minimum:3 }, :format => { :with => /\A[a-zA-Z\s]+\z/ }, :uniqueness => { :message => "Este pais ya se ha almacenado"}
 end
