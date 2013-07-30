5d1ac832e15f29f95cdbbad117774455e5b2ee1e
pedrodamiangv <pedrodamiangv@gmail.com>
2013-07-30 11:41:40 -0400

    Validacion de ppppppppp
    Al validar borra si es que se ingresa mas de dos caracteres iguales

--- sist_academico/app/assets/javascripts/application.js
+++ sist_academico/app/assets/javascripts/application.js
@@ -26,11 +26,15 @@
 
 
 
-function validarText(e,txt) {
-  if (txt.length > 1){
+function validarText(e,div) {
+  txt = $(div).val();
+  if (txt.length > 2){
     chart_one = txt.charAt(txt.length - 1);
-    chart_two = txt.charAt(txt.length -2);
-    return !(chart_one == chart_two);
+    chart_two = txt.charAt(txt.length - 2);
+    chart_three = txt.charAt(txt.length - 3);
+    if (chart_one == chart_two && chart_one == chart_three){
+      $(div).val(txt.substring(0, txt.length-1));
+    }
   }
   return true;
 }
